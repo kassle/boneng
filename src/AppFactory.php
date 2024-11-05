bda402322655cacb00fd368c16dcf89ee2f5b28b
erdian <erdian@rayslan.com>
2024-11-05 08:51:38 +0700

    enable custom decoder

--- src/AppFactory.php
+++ src/AppFactory.php
@@ -2,15 +2,18 @@
 
 namespace Boneng;
 
+use Boneng\Codex\Decoder;
 use Boneng\Codex\HttpDecoder;
 use Boneng\Processor\JsonRenderer;
 use Boneng\Processor\Renderer;
 use Boneng\Processor\SystemLogger;
+use Psr\Log\LoggerInterface;
+
 
 class AppFactory {
     private function __construct() { }
 
-    public static function create(string $appName, Renderer $htmlRenderer) : App {
-        return new AppImpl(new HttpDecoder(), $htmlRenderer, new JsonRenderer(), new SystemLogger($appName));
+    public static function create(string $appName, Renderer $htmlRenderer, Decoder $decoder = new HttpDecoder()) : App {
+        return new AppImpl($decoder, $htmlRenderer, new JsonRenderer(), new SystemLogger($appName));
     }
 }
